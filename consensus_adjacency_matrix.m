fa0970572728e8d9c549fc59f6d362bb975858fd
jaketomcko <jtomcko@gmail.com>
2023-11-23 14:34:23 -0500

    uploading base files
    * files given by apsc 200
    * slight modifications on leader.m

---
+++ consensus_adjacency_matrix.m
@@ -0,0 +1,15 @@
+function A = consensus_adjacency_matrix(y)
+%{
+Let "n" be the number of agents.
+
+--- Inputs ---
+y: n by 2 array holding output data for all agents
+   e.g., y(j,2) is the second output for agent-j
+
+--- Outputs ---
+A:  n by n adjacency matrix - see notes for details on construction
+%}
+
+
+
+end
